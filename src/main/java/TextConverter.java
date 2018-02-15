7ac5f362d8757ca07e5dd445c03565a766674582
heghinev <helenvanetsyan@gmail.com>
2018-02-15 11:23:22 +0400

    Text is seperated to lines with 4 words each. still needs to be improved

--- src/main/java/TextConverter.java
+++ src/main/java/TextConverter.java
@@ -1,38 +1,37 @@
 public class TextConverter {
-    public static String defaultText = "Lorem Ipsum is simply dummy text of the printing"
-            + " and typesetting industry. Lorem Ipsum has been the industry's standard ";
-            /*+ "dummy text ever since the 1500s, when an unknown printer took a galley of "
+    private static String defaultText = "Lorem Ipsum is simply dummy text of the printing"
+            + " and typesetting industry. Lorem Ipsum has been the industry's standard "
+            + "dummy text ever since the 1500s, when an unknown printer took a galley of "
             + "type and scrambled it to make a type specimen book. It has survived not only five centuries, "
             + "but also the leap into electronic typesetting, remaining essentially unchanged. "
             + "It was popularised in the 1960s with the release of Letraset sheets containing "
             + "Lorem Ipsum passages, and more recently with desktop publishing software like "
-            + "Aldus PageMaker including versions of Lorem Ipsum.";*/
+            + "Aldus PageMaker including versions of Lorem Ipsum.";
 
 
     static String textLine = "";
-    static int maxLineLength = 21;
+    //static int maxLineLength = 21;
     static int lineCount = 0;
-    static int wordCount = 0;
+    //static int wordCount = 0;
 
     public static String getFinalText(String defaultText) {
         if(lineCount < 4) {
-            if (textLine.length() < maxLineLength) {
-                if (wordCount < 4) {
+            for (int maxLineLength = 21; textLine.length() < maxLineLength; lineCount++) {
+                for (int wordCount = 0; wordCount < 4; wordCount++){
                     int indexOfSpace = defaultText.indexOf(" ");
                     textLine = textLine + " " + defaultText.substring(0, indexOfSpace);
+                    System.out.println(indexOfSpace);
                     defaultText = defaultText.substring(indexOfSpace + 1);
-                    wordCount++;
-
                 }System.out.println(textLine);
                 textLine = "";
                 /*else{
                     wordCount = 0;
                     System.out.println(textLine + " \n");
                 }*/
-            }lineCount++;
+            }
 
         }else {
-            System.out.println(textLine);
+            //System.out.println(textLine);
             System.out.println("*-*-*-*-*-*-*-*-*-*");
             lineCount = 0;
         }
